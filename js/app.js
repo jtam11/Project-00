dfa88b1f13b4e3722e8c75643dce0182b0a198d0
jtam11 <justinktam@gmail.com>
2016-03-26 02:59:35 -0700

    Added win condition for each player. Reset re-enables keydown after a player wins.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,55 @@
-
+var player1 = $('#one');
+var player2 = $('#two');
+var isKeyOn = true;
 
 $(document).on('ready', function () {
-
-  var player1 = $('#one');
-  var player2 = $('#two');
 
   console.log('JS is loaded!');
 
   /* Moving Player 1 */
   $(document).on('keydown', function (event) {
-   if (event.keyCode === 65) {
-     player1.animate({
-     left: "+=20px"
-   }, 50);
-   }
+    if (event.keyCode === 65 && isKeyOn) {
+      player1.animate({
+      left: "+=20px"
+      }, 50);
+      oneWon();
+      console.log(isKeyOn);
+    }
   });
 
   /* Moving Player 2 */
   $(document).on('keydown', function (event) {
-   if (event.keyCode === 74) {
-     player2.animate({
-     left: "+=20px"
-   }, 50);
-   }
-  });
-
-  /* Reset Button */
-  $('#reset').on('click', function resetRace() {
-    player1.animate({left: '18px'}, 50);
-    player2.animate({left: '18px'}, 50);
+    if (event.keyCode === 74 && isKeyOn) {
+      player2.animate({
+      left: "+=20px"
+      }, 50);
+      twoWon();
+    }
   });
 });
+
+/* Reset Button */
+$('#reset').on('click', function resetRace() {
+  player1.animate({left: '18px'}, 50);
+  player2.animate({left: '18px'}, 50);
+  isKeyOn = true;
+});
+
+/* Player 1 Win Verifier */
+function oneWon() {
+  if( parseInt( $('#one').css('left') ) >= 900 ) {
+    alert('Player One Wins!');
+    isKeyOn = false;
+  }
+}
+
+/* Player 2 Win Verifier */
+function twoWon() {
+  if( parseInt( $('#two').css('left') ) >= 900 ) {
+    alert('Player Two Wins!');
+    isKeyOn = false;
+  }
+}
 
 
 // function Sprite(name, color) {
